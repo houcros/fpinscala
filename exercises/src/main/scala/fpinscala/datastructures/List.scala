32307c5470a951c277035bfc16acbcd47f0cb561
Ignacio Lucero <houcros@hotmail.com>
2021-03-29 00:40:15 +0200

    Implement remaining exercises from chapter 1.

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
@@ -50,19 +52,92 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, t) => t
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] =
+    Cons(h, l)
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => l
+      case Cons(_, t) => drop(t, n - 1)
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  @tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => l
+    case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = {
+    @tailrec
+    def go(l: List[A])(acc: List[A]): List[A] = l match {
+      case Nil => Nil
+      case Cons(_, Nil) => acc
+      case Cons(h, t) => go(t)(Cons(h, acc))
+    }
+    go(l)(Nil)
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = {
+    @tailrec
+    def go(l: List[A], n: Int): Int = l match {
+      case Nil => n
+      case Cons(_, t) => go(t, n + 1)
+    }
+    go(l, 0)
+  }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  @tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, f(z, h))(f)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  // This is how make reverse tail-recursive!
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, Nil: List[A]) { (b, a) => Cons(a, b)}
+    //foldRightTailRec(l, Nil: List[A]) { (a, b) => Cons(a, b) }
+
+  // The straight-forward implementation of foldRight is not tail-recursive (stack-safe).
+  // But foldRight is! So we can make foldRight tail-recursive leveraging foldLeft.
+  def foldRightTR[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(reverse(as), z) { (b, a) => f(a, b) }
+
+  def append2[A](l1: List[A], l2: List[A]): List[A] =
+    foldRightTR(l1, l2)(Cons(_, _))
+
+  def concatenate[A](ls: List[List[A]]): List[A] =
+    foldRightTR(ls, Nil: List[A]) { (l, acc) => append2(l, acc) }
+
+  // Not stack-safe. Can be made stack-safe using foldRightTR.
+  def map[A, B](l: List[A])(f: A => B): List[B] = l match {
+    case Nil => Nil
+    case Cons(h, t) => Cons(f(h), map(t)(f))
+  }
+
+  // Same than for map.
+  def filter[A, B](l: List[A])(p: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) if p(h) => Cons(h, filter(t)(p))
+    case Cons(_, t) => filter(t)(p)
+  }
+
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] =
+    concatenate(map(l)(f))
+
+  def filter2[A, B](l: List[A])(p: A => Boolean): List[A] =
+    flatMap(l) { a => if (p(a)) List(a) else Nil }
+
+  def zipWith[A](l1: List[A])(l2: List[A])(f: (A, A) => A): List[A] =
+    (l1, l2) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1)(t2)(f))
+    }
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,49 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(left, right) => 1 + size(left) + size(right)
+  }
 
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(value) => value
+    case Branch(left, right) => maximum(left) max maximum(right)
+  }
 
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(left, right) => 1 + (depth(left) max depth(right))
+  }
 
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(value) => Leaf(f(value))
+    case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+  }
 
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(value) => f(value)
+    case Branch(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
+  }
+
+  def sizeF[A](t: Tree[A]): Int = fold(t)(_ => 1) { case (l, r) => 1 + l + r }
+
+  def maximumF(t: Tree[Int]): Int = fold(t)(identity)(_ max _)
+
+  def depthF[A](t: Tree[A]): Int = fold(t)(_ => 0) { case (l, r) => 1 + (l max r) }
+
+  def mapF[A, B](t: Tree[A])(f: A => B): Tree[B] = fold[A, Tree[B]](t)(x => Leaf(f(x))) { case (l, r) => Branch(l, r) }
+
+  def main(args: Array[String]): Unit = {
+    val t = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+
+    assert(size(t) == 5)
+    assert(maximum(t) == 3)
+    assert(depth(t) == 2)
+    assert(map(t)(_ + 1) == Branch(Branch(Leaf(2), Leaf(3)), Leaf(4)))
+    assert(sizeF(t) == 5)
+    assert(maximumF(t) == 3)
+    assert(depthF(t) == 2)
+    assert(mapF(t)(_ + 1) == Branch(Branch(Leaf(2), Leaf(3)), Leaf(4)))
+  }
 }
